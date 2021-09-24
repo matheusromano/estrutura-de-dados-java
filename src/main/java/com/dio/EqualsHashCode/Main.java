a4c0368a97766b558d21f75353a91e232698ccd6
matheusromano <romanobraga.m@gmail.com>
2021-09-24 14:09:57 -0300

    first commit

---
+++ src/main/java/com/dio/EqualsHashCode/Main.java
@@ -0,0 +1,27 @@
+package com.dio.EqualsHashCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        List<Carro> listaCarros = new ArrayList<>();
+
+        listaCarros.add(new Carro("Ford"));
+        listaCarros.add(new Carro("Chevrolet"));
+        listaCarros.add(new Carro("Volkswagen"));
+
+        System.out.println(listaCarros.contains(new Carro("Ford")));
+        System.out.println(new Carro("Ford").hashCode());
+        System.out.println(new Carro("Ford").hashCode());
+
+        Carro carro1 = new Carro("Ford");
+        Carro carro2 = new Carro("Audi");
+
+        System.out.println(carro1.equals(carro2));
+
+    }
+
+}
